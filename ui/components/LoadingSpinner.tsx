7e78d438c05d55ae21bed10ec3918977a2867a1a
jettblu <jettblu.fabrications@gmail.com>
2023-07-14 20:19:05 -0600

    Add loading spinner component

---
+++ ui/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+// loading spinner component
+
+export default function LoadingSpinner() {
+  return (
+    <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-b-2 border-gray-900 dark:border-gray-100 mx-2"></div>
+  );
+}
