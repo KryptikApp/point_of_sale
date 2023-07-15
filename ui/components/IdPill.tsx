d5fe61edfddfb8c7efba1998a2cf3303067a9baf
jettblu <jettblu.fabrications@gmail.com>
2023-07-14 20:21:58 -0600

    Add identity pill

---
+++ ui/components/IdPill.tsx
@@ -0,0 +1,22 @@
+import toast from "react-hot-toast";
+import { useThemeContext } from "./ThemeProvider";
+import { useAuthContext } from "./AuthProvider";
+import { trimPrincipal } from "../utils/identity";
+
+export default function IdPill() {
+  const { merchant } = useAuthContext();
+  const { primaryColor } = useThemeContext();
+  const handleCopy = () => {
+    navigator.clipboard.writeText(merchant?.id || "");
+    toast.success("ID copied to clipboard.");
+  };
+  return (
+    <div
+      className="flex flex-row space-x-2 px-2 py-1 rounded-lg dark:bg-gray-700/60 bg-gray-200/60 hover:cursor-pointer text-2xl w-fit hover:text-green-400"
+      style={{ borderColor: primaryColor }}
+      onClick={() => handleCopy()}
+    >
+      {trimPrincipal(merchant?.id || "")}
+    </div>
+  );
+}
