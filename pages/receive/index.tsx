81641b9f0c599fbaf3f814d44d17e94059157dec
jettblu <jettblu.fabrications@gmail.com>
2023-07-14 20:22:35 -0600

    Add receive page

---
+++ pages/receive/index.tsx
@@ -0,0 +1,24 @@
+import { useAuthContext } from "@/ui/components/AuthProvider";
+import QRCode from "@/ui/components/QrCode";
+import { useThemeContext } from "@/ui/components/ThemeProvider";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+
+export default function IdPill() {
+  const { merchant } = useAuthContext();
+  const router = useRouter();
+  useEffect(() => {
+    if (!merchant) {
+      router.push("/");
+    }
+  }, [merchant]);
+  return (
+    <div>
+      {merchant?.id && <QRCode text={merchant?.id} />}
+      {!merchant?.id && (
+        <p className="text-red-500">Merchant ID not available.</p>
+      )}
+    </div>
+  );
+}
