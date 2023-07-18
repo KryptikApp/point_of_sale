5c9fdfcfa6e440024a278f2d071129a437f75733
jettblu <jettblu.fabrications@gmail.com>
2023-07-18 01:17:33 -0600

    Update color preferences

--- ui/components/Layout.tsx
+++ ui/components/Layout.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import ColorPicker from "./ColorPicker";
+import Menu from "./Nav/Menu";
+import Navbar from "./Nav/Navbar";
 
 // TODO: Update to support dynamic headers
 export default function Layout({ children }: any) {
@@ -17,12 +19,14 @@ export default function Layout({ children }: any) {
         className={`
         px-2`}
       >
-        {/* top right color picker */}
-        <div className="relative h-[10vh]">
-          <div className="fixed right-2 md:right-4">
+        <Navbar />
+        {/* bottom center color picker */}
+        <div className="relative h-[5vh]">
+          <div className="fixed bottom-10 inset-x-0 max-w-max mx-auto">
             <ColorPicker />
           </div>
         </div>
+        <div className="min-h-[10vh] md:min-h-[4vh]" />
         {children}
         <div className="min-h-[10vh] md:min-h-0"></div>
         <Toaster />
--- ui/constants/colors.ts
+++ ui/constants/colors.ts
@@ -1,3 +1,5 @@
 export const BLUE = "#1e88e5";
 export const YELLOW = "#FFEC60";
-export const PURPLE = "#61A8FF";
+export const PURPLE = "#7B47F5";
+export const LIGHT_PURPLE = "#D247F5";
+export const ORANGE = "#F57B47";
