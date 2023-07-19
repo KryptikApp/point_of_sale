dc42b963fb40de8a0e5585dfc05d9ad168b1f5df
jettblu <jettblu.fabrications@gmail.com>
2023-07-19 01:01:58 -0600

    Update merchant backend declerations

--- ui/declarations/merchant_backend/merchant_backend.did.d.ts
+++ ui/declarations/merchant_backend/merchant_backend.did.d.ts
@@ -3,9 +3,16 @@ import type { ActorMethod } from '@dfinity/agent';
 
 export interface Main {
   'get' : ActorMethod<[], Response>,
+  'getMerchantBySlug' : ActorMethod<[string], Response>,
+  'isSlugAvailable' : ActorMethod<[string], ResponseBool>,
+  'sendSMS' : ActorMethod<[string, string], string>,
+  'txMessage' : ActorMethod<[string, string, string], ResponseMessage>,
   'update' : ActorMethod<[Merchant], Response>,
+  'welcomeMessage' : ActorMethod<[string, string], ResponseMessage>,
 }
 export interface Merchant {
+  'id' : string,
+  'slug' : string,
   'businessName' : string,
   'phoneNumber' : string,
   'phoneNotifications' : boolean,
@@ -13,6 +20,18 @@ export interface Merchant {
 export interface Response {
   'status' : number,
   'data' : [] | [Merchant],
+  'status_text' : string,
+  'error_text' : [] | [string],
+}
+export interface ResponseBool {
+  'status' : number,
+  'data' : boolean,
+  'status_text' : string,
+  'error_text' : [] | [string],
+}
+export interface ResponseMessage {
+  'status' : number,
+  'data' : [] | [string],
   'status_text' : string,
   'error_text' : [] | [string],
 }
--- ui/declarations/merchant_backend/merchant_backend.did.js
+++ ui/declarations/merchant_backend/merchant_backend.did.js
@@ -1,5 +1,7 @@
 export const idlFactory = ({ IDL }) => {
   const Merchant = IDL.Record({
+    'id' : IDL.Text,
+    'slug' : IDL.Text,
     'businessName' : IDL.Text,
     'phoneNumber' : IDL.Text,
     'phoneNotifications' : IDL.Bool,
@@ -10,10 +12,31 @@ export const idlFactory = ({ IDL }) => {
     'status_text' : IDL.Text,
     'error_text' : IDL.Opt(IDL.Text),
   });
+  const ResponseBool = IDL.Record({
+    'status' : IDL.Nat16,
+    'data' : IDL.Bool,
+    'status_text' : IDL.Text,
+    'error_text' : IDL.Opt(IDL.Text),
+  });
+  const ResponseMessage = IDL.Record({
+    'status' : IDL.Nat16,
+    'data' : IDL.Opt(IDL.Text),
+    'status_text' : IDL.Text,
+    'error_text' : IDL.Opt(IDL.Text),
+  });
   const Main = IDL.Service({
     'get' : IDL.Func([], [Response], ['query']),
+    'getMerchantBySlug' : IDL.Func([IDL.Text], [Response], []),
+    'isSlugAvailable' : IDL.Func([IDL.Text], [ResponseBool], []),
+    'sendSMS' : IDL.Func([IDL.Text, IDL.Text], [IDL.Text], []),
+    'txMessage' : IDL.Func(
+        [IDL.Text, IDL.Text, IDL.Text],
+        [ResponseMessage],
+        [],
+      ),
     'update' : IDL.Func([Merchant], [Response], []),
+    'welcomeMessage' : IDL.Func([IDL.Text, IDL.Text], [ResponseMessage], []),
   });
   return Main;
 };
-export const init = ({ IDL }) => { return []; };
+export const init = ({ IDL }) => { return [IDL.Text, IDL.Text]; };
