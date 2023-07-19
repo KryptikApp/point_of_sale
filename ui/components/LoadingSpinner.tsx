2fe10db55570db5823e95e518ea6e097b912600b
jettblu <jettblu.fabrications@gmail.com>
2023-07-19 01:08:00 -0600

    Update spinner style

--- ui/components/LoadingSpinner.tsx
+++ ui/components/LoadingSpinner.tsx
@@ -2,6 +2,6 @@
 
 export default function LoadingSpinner() {
   return (
-    <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-b-2 border-gray-900 dark:border-gray-100 mx-2"></div>
+    <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-b-2 border-gray-800/80 dark:border-gray-100/80 mx-2 my-auto"></div>
   );
 }
