0b8c290bcdf4af1b250503eb791af30af63937fb
jettblu <jettblu.fabrications@gmail.com>
2023-07-19 01:08:55 -0600

    Add explorer urls

---
+++ ui/constants/explorers.ts
@@ -0,0 +1,4 @@
+// base url for internet computer explorer
+export const EXPLORER_IC = "https://dashboard.internetcomputer.org";
+// base url for internet computer data requests
+export const API_IC = "https://icrc-api.internetcomputer.org/api/v1";
